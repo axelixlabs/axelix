f8beb299494bc81aac94428fe8f4dd4784305af8
Михаил Поливаха <mikhail.polivakha@gazprombank.ru>
2025-08-14 14:11:22 +0300

    polishing

--- master/src/main/java/com/nucleonforge/axile/master/api/BeansApi.java
+++ master/src/main/java/com/nucleonforge/axile/master/api/BeansApi.java
@@ -1,9 +1,10 @@
 package com.nucleonforge.axile.master.api;
 
-import com.nucleonforge.axile.master.api.response.BeansFeedResponse;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.nucleonforge.axile.master.api.response.BeansFeedResponse;
 
 /**
  * The API for managing beans.
--- master/src/main/java/com/nucleonforge/axile/master/domain/SpringBean.java
+++ master/src/main/java/com/nucleonforge/axile/master/domain/SpringBean.java
@@ -1,12 +1,8 @@
 package com.nucleonforge.axile.master.domain;
 
-import java.util.List;
-
 /**
  * The bean inside the Spring application.
  *
  * @author Mikhail Polivakha
  */
-public class SpringBean extends JavaObject {
-
-}
+public class SpringBean extends JavaObject {}
