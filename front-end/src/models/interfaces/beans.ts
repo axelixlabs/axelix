b76b6d792360bb7ceafe0c7a18511e822132e15a
Ashot <ashotsargsyan527@gmail.com>
2025-09-26 11:43:58 +0400

    GH-103 Fixes after code review (103)

--- front-end/src/models/interfaces/beans.ts
+++ front-end/src/models/interfaces/beans.ts
@@ -1,15 +1,46 @@
+import type { ICommonSliceState } from "./globals";
+
 export interface IBean {
+  /**
+   * Name of single bean
+   */
   beanName: string;
+  /**
+   * Name of single bean scope
+   */
   scope: string;
+  /**
+   * Name of single bean classname
+   */
   className: string;
+  /**
+   * Bean aliases list
+   */
   aliases: string[];
+  /**
+   * Bean dependecies
+   */
   dependencies: string[];
 }
 
-export interface IBeansSliceData {
-  loading: boolean;
-  error: string;
+export interface IBeansSliceState extends ICommonSliceState {
+  /**
+   * Full list of beans
+   */
   beans: IBean[];
+  /**
+   * Search text used for filtering beans
+   */
   beansSearchText: string;
+  /**
+   * Filtered beans after searching
+   */
   filteredBeans: IBean[];
+}
+
+export interface IBeansCollapseHeaderRefs {
+  /**
+   * Refs to bean collapse headers, used for smooth scrolling.
+   */
+  [key: string]: HTMLDivElement | null;
 }
---
+++ front-end/src/pages/Beans/BeansCollapse/BeanCollapseLabels/index.tsx
@@ -0,0 +1,37 @@
+import type { JSX, RefObject } from "react";
+import type { IBean, IBeansCollapseHeaderRefs } from "models";
+
+import styles from "./styles.module.css";
+
+interface IProps {
+  /**
+   * Single bean
+   */
+  bean: IBean;
+  /**
+   * Ref to bean collapse headers, used for smooth scrolling.
+   */
+  headerRefs: RefObject<IBeansCollapseHeaderRefs>;
+}
+
+export const BeanCollapseLabels = ({
+  bean,
+  headerRefs,
+}: IProps): JSX.Element => {
+  const { beanName, className, scope } = bean;
+
+  return (
+    <div
+      className={styles.CollapseHeader}
+      ref={(el) => {
+        headerRefs.current[beanName] = el;
+      }}
+    >
+      <div>
+        <p>{beanName}</p>
+        <p>{className}</p>
+      </div>
+      <div className={styles.ScopeWrapper}>{scope}</div>
+    </div>
+  );
+};
