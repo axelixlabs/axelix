bf41f8424feababc159658dddba358de9b4b7f41
Ashot <ashotsargsyan527@gmail.com>
2025-10-03 14:55:15 +0400

    GH-119 Fixes after review

--- front-end/src/pages/Loggers/Logger/index.tsx
+++ front-end/src/pages/Loggers/Logger/index.tsx
@@ -1,18 +1,20 @@
-import { ConfigProvider, Radio } from "antd";
+import { Tooltip } from "antd";
 import { useTranslation } from "react-i18next";
 
 import { setLoggerLevelThunk } from "store/slices";
-import { LoggerTruncat } from "./LoggerTruncat";
+import { LoggerName } from "./LoggerName";
 import { useAppDispatch } from "hooks";
 import type { ILogger } from "models";
 import { statePalette } from "utils";
+
+import TargetIcon from 'assets/icons/TargetIcon.svg'
 
 import styles from "./styles.module.css";
 
 interface IProps {
   /**
    * Levels for all loggers
-   */
+  */
   levels: string[];
   /**
    * Single logger
@@ -23,165 +25,58 @@ interface IProps {
 export const Logger = ({ levels, logger }: IProps) => {
   const { t } = useTranslation();
   const dispatch = useAppDispatch();
+  const { effectiveLevel, configuredLevel } = logger
 
-  const radioGroupColor = logger.configuredLevel
-    ? // @ts-expect-error todo fix types in future
-      statePalette[logger.configuredLevel]
-    : // @ts-expect-error todo fix types in future
-      statePalette[`${logger.effectiveLevel}Light`];
+  const handleChange = (level: string): void => {
+    if (configuredLevel === level) {
+      return
+    }
+
+    dispatch(setLoggerLevelThunk(level));
+  };
 
   return (
     <div className={styles.MainWrapper}>
-      <LoggerTruncat logger={logger} />
+      <LoggerName name={logger.name} />
 
       <div className={styles.LoggerValuesWrapper}>
-        <ConfigProvider
-          theme={{
-            components: {
-              Radio: radioGroupColor,
-            },
-          }}
-        >
-          <Radio.Group
-            defaultValue={logger.effectiveLevel}
-            buttonStyle="solid"
-            onChange={() => dispatch(setLoggerLevelThunk(""))}
-            className={styles.RadioGroup}
-          >
-            {levels.map((level) => (
-              <Radio.Button value={level} key={level}>
-                {level}
-              </Radio.Button>
-            ))}
-          </Radio.Group>
-        </ConfigProvider>
-        <button className={styles.Reset}>{t("reset")}</button>
+        <div className={styles.LevelsWrapper}>
+          {levels.map((level) => {
+            // @ts-expect-error todo fix type in future
+            const color = statePalette[level] || statePalette.DEFAULT;
+
+            return (
+              <div className={styles.RadioGroupWrapper}>
+                <label
+                  key={level}
+                  className={`${styles.RadioButton} ${effectiveLevel === level ? styles.Selected : ""}`}
+                  onClick={() => handleChange(level)}
+                  style={{
+                    "--color-primary": color.colorPrimary,
+                    "--color-primary-hover": color.colorPrimaryHover,
+                    "--color-primary-active": color.colorPrimaryActive,
+                  } as React.CSSProperties}
+                >
+                  <input
+                    type="radio"
+                    value={level}
+                    checked={effectiveLevel === level}
+                  />
+                  {level}
+                </label>
+                {configuredLevel === level && (
+                  <Tooltip title={t("configuredExplicitly")} className={styles.Tooltip}>
+                    <img src={TargetIcon} alt="" className={styles.TargetIcon} />
+                  </Tooltip>
+                )}
+              </div>
+            );
+          })}
+        </div>
+        <button className={styles.Reset}>
+          {t("reset")}
+        </button>
       </div>
     </div>
   );
 };
-
-// todo delete in the future if not needed
-
-// import { useState } from "react";
-// import { ConfigProvider, Radio } from "antd";
-// import { useTranslation } from "react-i18next";
-
-// import { LoggerTruncat } from "./LoggerTruncat";
-// import type { ILogger } from "models";
-
-// import styles from "./styles.module.css";
-// import { useAppDispatch } from "hooks";
-// import { setLoggerLevelThunk } from "store/slices";
-
-// interface IProps {
-//   levels: string[];
-//   logger: ILogger;
-// }
-
-// export const Logger = ({ levels, logger }: IProps) => {
-//   const { t } = useTranslation();
-//   const dispatch = useAppDispatch();
-
-//   const [selectedLevel, setSelectedLevel] = useState<string>("");
-
-//   const statePalette: Record<string, Record<string, string>> = {
-//     OFF: {
-//       colorPrimary: "#000000",
-//       colorPrimaryHover: "#1a1a1a",
-//       colorPrimaryActive: "#333333",
-//     },
-//     OFFLight: {
-//       colorPrimary: "#4d4d4d",
-//       colorPrimaryHover: "#666666",
-//       colorPrimaryActive: "#808080",
-//     },
-//     ERROR: {
-//       colorPrimary: "#ff4d4f",
-//       colorPrimaryHover: "#ff7875",
-//       colorPrimaryActive: "#d9363e",
-//     },
-//     ERRORLight: {
-//       colorPrimary: "#ff999a",
-//       colorPrimaryHover: "#ffb3b4",
-//       colorPrimaryActive: "#ff8081",
-//     },
-//     WARN: {
-//       colorPrimary: "#faad14",
-//       colorPrimaryHover: "#ffd666",
-//       colorPrimaryActive: "#d48806",
-//     },
-//     WARNLight: {
-//       colorPrimary: "#ffe08c",
-//       colorPrimaryHover: "#fff2b3",
-//       colorPrimaryActive: "#ffd34d",
-//     },
-//     INFO: {
-//       colorPrimary: "#1890ff",
-//       colorPrimaryHover: "#69c0ff",
-//       colorPrimaryActive: "#096dd9",
-//     },
-//     INFOLight: {
-//       colorPrimary: "#a3d0ff",
-//       colorPrimaryHover: "#cce6ff",
-//       colorPrimaryActive: "#70b8ff",
-//     },
-//     DEBUG: {
-//       colorPrimary: "#52c41a",
-//       colorPrimaryHover: "#95de64",
-//       colorPrimaryActive: "#389e0d",
-//     },
-//     DEBUGLight: {
-//       colorPrimary: "#b7eb8f",
-//       colorPrimaryHover: "#dcf5b6",
-//       colorPrimaryActive: "#85c441",
-//     },
-//     TRACE: {
-//       colorPrimary: "#722ed1",
-//       colorPrimaryHover: "#b37feb",
-//       colorPrimaryActive: "#531dab",
-//     },
-//     TRACELight: {
-//       colorPrimary: "#c6a9f3",
-//       colorPrimaryHover: "#e3d0fb",
-//       colorPrimaryActive: "#a077e0",
-//     },
-//   };
-
-//   return (
-//     <div className={styles.LoggerWrapper}>
-//       <LoggerTruncat logger={logger} />
-
-//       <div className={styles.LoggerValuesWrapper}>
-//         <ConfigProvider
-//           theme={{
-//             components: {
-//               Radio: selectedLevel
-//                 ? statePalette[selectedLevel]
-//                 : logger.configuredLevel
-//                 ? statePalette[logger.configuredLevel]
-//                 : statePalette[`${logger.effectiveLevel}Light`],
-//             },
-//           }}
-//         >
-//           <Radio.Group
-//             defaultValue={logger.effectiveLevel}
-//             buttonStyle="solid"
-//             onChange={(e) => {
-//               setSelectedLevel(e.target.value);
-//               dispatch(setLoggerLevelThunk(""));
-//             }}
-//             className={styles.RadioGroup}
-//           >
-//             {levels.map((level) => (
-//               <Radio.Button value={level} key={level}>
-//                 {level}
-//               </Radio.Button>
-//             ))}
-//           </Radio.Group>
-//         </ConfigProvider>
-//         <button className={styles.Reset}>{t("reset")}</button>
-//       </div>
-//     </div>
-//   );
-// };
--- front-end/src/utils/loggersColors.ts
+++ front-end/src/utils/loggersColors.ts
@@ -1,62 +1,87 @@
 export const statePalette = {
   OFF: {
-    colorPrimary: "#1c1c1c",
-    colorPrimaryHover: "#2e2e2e",
-    colorPrimaryActive: "#0f0f0f",
-  },
-  OFFLight: {
-    colorPrimary: "#666666",
-    colorPrimaryHover: "#808080",
-    colorPrimaryActive: "#4d4d4d",
+    colorPrimary: "#838383",
+    colorPrimaryHover: "#d9d9d9",
+    colorPrimaryActive: "#595959",
   },
   ERROR: {
     colorPrimary: "#ff4d4f",
     colorPrimaryHover: "#ff7875",
     colorPrimaryActive: "#d9363e",
   },
-  ERRORLight: {
-    colorPrimary: "#ffb3b3",
-    colorPrimaryHover: "#ffc2c2",
-    colorPrimaryActive: "#ff9999",
-  },
   WARN: {
     colorPrimary: "#faad14",
     colorPrimaryHover: "#ffd666",
     colorPrimaryActive: "#d48806",
   },
-  WARNLight: {
-    colorPrimary: "#fff0b3",
-    colorPrimaryHover: "#fff5cc",
-    colorPrimaryActive: "#ffe066",
-  },
   INFO: {
+    colorPrimary: "#fadb14",
+    colorPrimaryHover: "#ffec3d",
+    colorPrimaryActive: "#d4b106",
+  },
+  DEBUG: {
     colorPrimary: "#1890ff",
     colorPrimaryHover: "#69c0ff",
     colorPrimaryActive: "#096dd9",
   },
-  INFOLight: {
-    colorPrimary: "#b3e0ff",
-    colorPrimaryHover: "#d9f0ff",
-    colorPrimaryActive: "#85caff",
+  TRACE: {
+    colorPrimary: "#13c2c2",
+    colorPrimaryHover: "#36e3e3",
+    colorPrimaryActive: "#0b8a8a",
   },
-  DEBUG: {
+  ALL: {
     colorPrimary: "#52c41a",
     colorPrimaryHover: "#95de64",
     colorPrimaryActive: "#389e0d",
   },
-  DEBUGLight: {
-    colorPrimary: "#c2f0a6",
-    colorPrimaryHover: "#e6f9cc",
-    colorPrimaryActive: "#99d160",
-  },
-  TRACE: {
+  DEFAULT: {
     colorPrimary: "#722ed1",
     colorPrimaryHover: "#b37feb",
     colorPrimaryActive: "#531dab",
   },
-  TRACELight: {
-    colorPrimary: "#d1baf7",
-    colorPrimaryHover: "#ede0ff",
-    colorPrimaryActive: "#b28cf2",
-  },
 };
+
+
+
+// todo Remove light colors in the future if they are not needed.
+
+// DEFAULTLight: {
+//   colorPrimary: "#d1baf7",
+//   colorPrimaryHover: "#ede0ff",
+//   colorPrimaryActive: "#b28cf2",
+// },
+// ALLLight: {
+//   colorPrimary: "#c2f0a6",
+//   colorPrimaryHover: "#e6f9cc",
+//   colorPrimaryActive: "#99d160",
+// },
+// TRACELight: {
+//   colorPrimary: "#b3f5f5",
+//   colorPrimaryHover: "#d9fcfc",
+//   colorPrimaryActive: "#85d6d6",
+// },
+// DEBUGLight: {
+//   colorPrimary: "#b3e0ff",
+//   colorPrimaryHover: "#d9f0ff",
+//   colorPrimaryActive: "#85caff",
+// },
+// INFOLight: {
+//   colorPrimary: "#fff7b3",
+//   colorPrimaryHover: "#fffecb",
+//   colorPrimaryActive: "#e6d974",
+// },
+// WARNLight: {
+//   colorPrimary: "#fff0b3",
+//   colorPrimaryHover: "#fff5cc",
+//   colorPrimaryActive: "#ffe066",
+// },
+// ERRORLight: {
+//   colorPrimary: "#ffb3b3",
+//   colorPrimaryHover: "#ffc2c2",
+//   colorPrimaryActive: "#ff9999",
+// },
+// OFFLight: {
+//   colorPrimary: "#666666",
+//   colorPrimaryHover: "#808080",
+//   colorPrimaryActive: "#4d4d4d",
+// },
