93aef19f5f73795000bc8ace170be751f3d853c3
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-10-13 21:46:10 +0300

    GH-203 tmp

--- front-end/src/models/interfaces/beans.ts
+++ front-end/src/models/interfaces/beans.ts
@@ -1,18 +1,49 @@
 import type { ICommonSliceState } from "./globals";
 
+export enum ProxyType {
+    JDK_PROXY = "JDK_PROXY",
+    CGLIB = "CGLIB",
+    NO_PROXYING = "NO_PROXYING",
+}
+
 export interface IBean {
+
   /**
    * Name of single bean
    */
   beanName: string;
+
   /**
    * Name of single bean scope
    */
   scope: string;
+
   /**
    * Name of single bean classname
    */
   className: string;
+
+  /**
+   * The proxying algorithm that is used for this bean. Might be null in case
+   * the backend was unable to figure it out.
+   */
+  proxyType: ProxyType | null;
+
+  /**
+   * Qualified of this bean, if any
+   */
+  qualifiers: string[];
+
+  /**
+   * Whether this bean is lazily initialized
+   */
+  isLazyInit: boolean;
+
+  /**
+   * Whether this bean is marked as primary
+   */
+  isPrimary: boolean;
+
   /**
    * Bean aliases list
    */
