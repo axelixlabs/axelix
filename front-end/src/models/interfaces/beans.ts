b1161c458dace1e7fb0aefd63661e9eb2d9de88b
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-10-14 14:09:31 +0300

    GH-203: Integrated additional bean info (excluding bean source)

--- front-end/src/models/interfaces/beans.ts
+++ front-end/src/models/interfaces/beans.ts
@@ -6,6 +6,9 @@ export enum ProxyType {
     NO_PROXYING = "NO_PROXYING",
 }
 
+/**
+ * An interface that represents the state of the particular bean inside the Spring Boot application
+ */
 export interface IBean {
 
   /**
@@ -19,7 +22,7 @@ export interface IBean {
   scope: string;
 
   /**
-   * Name of single bean classname
+   * Name of single bean class name
    */
   className: string;
 
@@ -48,11 +51,59 @@ export interface IBean {
    * Bean aliases list
    */
   aliases: string[];
+
   /**
    * Bean dependencies
    */
   dependencies: string[];
+
+  /**
+   * The source from which the bean came from.
+   */
+  beanSource: IBeanSource;
 }
+
+/**
+ * Represents the actual algorithm of how the Spring Framework found this bean
+ */
+export enum BeanOrigin {
+  BEAN_METHOD = "BEAN_METHOD",
+  COMPONENT_ANNOTATION = "COMPONENT_ANNOTATION",
+  FACTORY_BEAN = "FACTORY_BEAN",
+  UNKNOWN = "UNKNOWN",
+}
+
+/**
+ * The "source" of the bean. By "source" we mean how exactly this particular {@link IBean} was discovered
+ * by the Spring Framework
+ */
+export interface IBeanSource {
+
+  /**
+   * Optional name of the method that actually produces the instance of the given bean. Present in response from the
+   * backend only in case of {@link origin} is equal to {@link BeanOrigin.BEAN_METHOD}
+   */
+  methodName?: string;
+
+  /**
+   * Optional fully qualified name of the enclosing @Configuration class from which the bean came from. Not
+   * to be confused with {@link IBean.className}. Present in response from the backend only in case of {@link origin}
+   * is equal to {@link BeanOrigin.BEAN_METHOD}
+   */
+  enclosingClassName?: string;
+
+  /**
+   * Optional name of the factory bean name. Present in response from the server only if
+   * {@link origin} is equal to {@link BeanOrigin.FACTORY_BEAN}
+   */
+  factoryBeanName?: string;
+
+  /**
+   * The actual origin
+   */
+  origin: BeanOrigin;
+}
+
 
 export interface IBeansData {
   /**
---
+++ front-end/src/pages/Beans/BeansCollapse/BeanCollapseChildren/BeanSource/index.tsx
@@ -0,0 +1,23 @@
+// import type {IBeanSource} from "models";
+// import {Collapse} from "antd";
+// import styles from "../../styles.module.css";
+//
+// interface IProps {
+//
+//   /**
+//    * The source from which the bean came from.
+//    */
+//   beanSource: IBeanSource
+// }
+//
+// export const BeanSource = ( { beanSource } : IProps ) => {
+//
+//   return <Collapse
+//     accordion
+//     activeKey={activeKey}
+//     items={createCollapseItems(beans)}
+//     onChange={(key) => setActiveKey(key)}
+//     bordered
+//     className={styles.Collapse}
+//   />
+// }
