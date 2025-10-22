0a68a20df1a2954f4802bbc95b95cf330e4c5cf3
Ashot <ashotsargsyan527@gmail.com>
2025-10-22 16:58:17 +0400

    Gh-215 Added Prettier, set rules for imports and main code, code formatted

--- front-end/eslint.config.js
+++ front-end/eslint.config.js
@@ -1,23 +1,43 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
-  {
-    files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
-    ],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default [
+    {
+        plugins: {
+            "react-hooks": reactHooks,
+            react,
+            "react-refresh": reactRefresh,
+        },
     },
-  },
-])
+    {
+        ignores: ["dist", "node_modules"],
+    },
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    {
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+                ...globals.node,
+                ...globals.es2022,
+            },
+        },
+    },
+    {
+        files: ["**/*.{js, jsx, ts, tsx}"],
+    },
+    {
+        settings: {
+            "import/resolver": {
+                typescript: {
+                    project: "./tsconfig.json",
+                },
+            },
+        },
+    },
+];
