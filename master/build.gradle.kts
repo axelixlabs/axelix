b853fae0e5bb8bf59b5937a0a9dcb3602ffeb64f
Sergey Cherkasov <106945503+sergeycherkasovv@users.noreply.github.com>
2025-10-27 13:51:50 +0700

    GH-258: moved versions of common dependencies to the common module
    * GH-258: moved versions of common dependencies to the common module (#258)
    
    * GH-258: minor changes (#258)
    
    * GH-258 minor changes
    
    ---------
    
    Co-authored-by: nikita-kirillov <kirilloffnikita1@gmail.com>

--- master/build.gradle.kts
+++ master/build.gradle.kts
@@ -4,12 +4,6 @@ plugins {
 }
 
 val testcontainersVersion = "1.21.3"
-
-// TODO: the JWT version also ideally needs to be extracted
-val jsonwebtokenVersion = "0.12.6"
-
-// TODO: instancio is 100% candidate for being extracted to 'shared'
-val instancioVersion = "5.5.1"
 val springDocSwaggerVersion = "2.0.4"
 
 dependencies {
@@ -24,15 +18,11 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.cloud:spring-cloud-kubernetes-fabric8-discovery")
     implementation("org.slf4j:slf4j-api")
-    implementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:${springDocSwaggerVersion}")
 
     // Runtime
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
     runtimeOnly("ch.qos.logback:logback-classic")
 
-    // Swagger(OpenAPI)
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springDocSwaggerVersion")
 
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -41,7 +31,6 @@ dependencies {
     testImplementation("org.postgresql:postgresql")
     testImplementation("com.squareup.okhttp3:mockwebserver")
     testImplementation("com.squareup.okhttp3:okhttp")
-    testImplementation("org.instancio:instancio-core:${instancioVersion}")
 }
 
 configurations.all {
--- sbs/auth/build.gradle.kts
+++ sbs/auth/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("sbs")
 }
 
-val jsonwebtokenVersion = "0.12.6"
-
 dependencies {
     // Self
     implementation(project(":common_auth"))
@@ -12,18 +10,8 @@ dependencies {
     compileOnly("org.springframework.boot:spring-boot-starter-web")
     compileOnly("org.springframework.boot:spring-boot-starter-actuator")
 
-    // Api
-    api("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
-
-    // Runtime
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
-
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-starter-web")
     testImplementation("org.springframework.boot:spring-boot-starter-actuator")
-    testImplementation("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
-    testRuntimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
-    testRuntimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
 }
