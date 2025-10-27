afdafbb4c06b1f853beb34e25b9c9df4dd098881
Sergey Cherkasov <106945503+sergeycherkasovv@users.noreply.github.com>
2025-10-27 13:51:50 +0700

    GH-258: moved versions of common dependencies to the common module
    * GH-258: moved versions of common dependencies to the common module (#258)
    
    * GH-258: minor changes (#258)
    
    * GH-258 minor changes
    
    ---------
    
    Co-authored-by: nikita-kirillov <kirilloffnikita1@gmail.com>

--- buildSrc/src/main/kotlin/shared.gradle.kts
+++ buildSrc/src/main/kotlin/shared.gradle.kts
@@ -1,20 +1,35 @@
-import Dependencies.jspecifyVersion
 import Dependencies.springBootVersion
 import Dependencies.springCloudDependenciesVersion
 import Dependencies.jsonUnitAssertJVersion
+import Dependencies.jspecifyVersion
+import Dependencies.instancioVersion
+import Dependencies.jsonwebtokenVersion
 
 plugins {
     id("java-library")
 }
 
 dependencies {
+    // Impl
     implementation(platform("org.springframework.boot:spring-boot-dependencies:${springBootVersion}"))
     implementation(platform("org.springframework.cloud:spring-cloud-dependencies:${springCloudDependenciesVersion}"))
+    implementation("org.jspecify:jspecify:${jspecifyVersion}")
 
+    // Api
+    api("io.jsonwebtoken:jjwt-api:${jsonwebtokenVersion}")
+
+    // Runtime
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:${jsonwebtokenVersion}")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:${jsonwebtokenVersion}")
+
+    // Test
     testImplementation(platform("org.springframework.boot:spring-boot-dependencies:$springBootVersion"))
     testImplementation(platform("org.springframework.cloud:spring-cloud-dependencies:$springCloudDependenciesVersion"))
     testImplementation("net.javacrumbs.json-unit:json-unit-assertj:$jsonUnitAssertJVersion")
-    implementation("org.jspecify:jspecify:${jspecifyVersion}")
+    testImplementation("org.instancio:instancio-core:${instancioVersion}")
+    testImplementation("io.jsonwebtoken:jjwt-api:${jsonwebtokenVersion}")
+    testRuntimeOnly("io.jsonwebtoken:jjwt-impl:${jsonwebtokenVersion}")
+    testRuntimeOnly("io.jsonwebtoken:jjwt-jackson:${jsonwebtokenVersion}")
 }
 
 java {
--- sbs/auto-configuration/build.gradle.kts
+++ sbs/auto-configuration/build.gradle.kts
@@ -2,8 +2,6 @@ plugins {
     id("sbs")
 }
 
-val jsonwebtokenVersion = "0.12.6"
-
 dependencies {
     // Self
     api(project(":common_auth"))
@@ -17,9 +15,4 @@ dependencies {
     compileOnly("org.springframework.cloud:spring-cloud-starter-openfeign")
     compileOnly("org.springframework.kafka:spring-kafka")
     compileOnly("com.zaxxer:HikariCP:6.3.0")
-    compileOnly("io.jsonwebtoken:jjwt-api:$jsonwebtokenVersion")
-
-    // Runtime
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:$jsonwebtokenVersion")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:$jsonwebtokenVersion")
 }
