67299afbe333a9c7ba177c399eb657b837b8fac0
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-10-31 00:02:22 +0300

    GH-290: Polishing Scheduled tasks API

--- front-end/src/pages/ScheduledTasks/Cron/CronTasks/index.tsx
+++ front-end/src/pages/ScheduledTasks/Cron/CronTasks/index.tsx
@@ -3,9 +3,9 @@ import { useTranslation } from "react-i18next";
 import { EmptyHandler } from "components";
 import type { ICron } from "models";
 
+import styles from "../../styles.module.css";
+import { CronTableHeader } from "../CronTableHeader";
 import { CronTaskTableRow } from "../CronTaskTableRow";
-import { TableHeader } from "../TableHeader";
-import styles from "../styles.module.css";
 
 interface IProps {
     /**
@@ -23,8 +23,7 @@ export const CronTasks = ({ cronTasks }: IProps) => {
                 <div className={`MediumTitle ${styles.TaskType}`}>{t("ScheduledTasks.cron")}</div>
 
                 <div className="CustomizedAntdTable">
-                    <TableHeader isCron={true} />
-
+                    <CronTableHeader />
                     <EmptyHandler isEmpty={cronTasks.length === 0}>
                         {cronTasks.map((task: ICron, index: number) => (
                             <CronTaskTableRow task={task} key={index} />
---
+++ front-end/src/pages/ScheduledTasks/FixedTasks/FixedTaskTableHeader/index.tsx
@@ -0,0 +1,16 @@
+import { useTranslation } from "react-i18next";
+
+import styles from "./styles.module.css";
+
+export const FixedTaskTableHeader = () => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={`TableHeader ${styles.TableHeader}`}>
+            <div className="RowChunk">{t("ScheduledTasks.runnable")}</div>
+            <div dangerouslySetInnerHTML={{ __html: t("ScheduledTasks.initialDelay") }} className="RowChunk" />
+            <div className="RowChunk">{t("ScheduledTasks.interval")}</div>
+            <div className={`RowChunk ${styles.Status}`}>{t("ScheduledTasks.status")}</div>
+        </div>
+    );
+};
