f1fdb09a6c737ea2bf96834e6e6af33455d3d1ec
Ashot Sargsyan <135041956+AshotSargsyan1@users.noreply.github.com>
2025-10-31 00:45:58 +0400

    GH-267 Draft of the details page (#286)

---
+++ front-end/src/pages/Details/index.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+import { Loader } from "components";
+import { fetchData } from "helpers";
+import { type IDetailsResponseBody, StatefulRequest } from "models";
+import { getDetailsData } from "services";
+
+import { DetailsCard } from "./DetailsCard";
+import styles from "./styles.module.css";
+
+const Details = () => {
+    const { instanceId } = useParams();
+
+    const [dataState, setDataState] = useState(StatefulRequest.loading<IDetailsResponseBody>());
+
+    useEffect(() => {
+        fetchData(setDataState, () => getDetailsData(instanceId!));
+    }, []);
+
+    if (dataState.loading) {
+        return <Loader />;
+    }
+
+    if (dataState.error) {
+        // todo change error handling in future
+        return dataState.error;
+    }
+
+    const detailsFeed = dataState.response!;
+    const { serviceName, git, build, spring, runtime, os } = detailsFeed;
+
+    // Here, the order is very important. It must be exactly like this:
+    // git, build, spring, runtime, os
+    const detailsCardsData: Omit<IDetailsResponseBody, "serviceName"> = {
+        git: git,
+        build: build,
+        spring: spring,
+        runtime: runtime,
+        os: os,
+    };
+
+    const getCardsData = Object.entries(detailsCardsData).map(([title, content]) => ({
+        title: title,
+        content: Object.entries(content),
+    }));
+
+    return (
+        <>
+            <div className={styles.MainTitle}>{serviceName}</div>
+
+            <div className={styles.InnerWrapper}>
+                <div className={styles.ColumnWrapper}>
+                    {getCardsData.slice(0, 2).map(({ title, content }) => (
+                        <DetailsCard title={title} content={content} key={title} />
+                    ))}
+                </div>
+
+                <div className={styles.ColumnWrapper}>
+                    {getCardsData.slice(2, 5).map(({ title, content }) => (
+                        <DetailsCard title={title} content={content} key={title} />
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Details;
