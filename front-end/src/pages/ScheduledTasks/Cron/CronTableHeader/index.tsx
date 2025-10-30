86094dac6596331ef56200181b215c9f916a397e
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-10-31 00:02:22 +0300

    GH-290: Polishing Scheduled tasks API

---
+++ front-end/src/pages/ScheduledTasks/Cron/CronTableHeader/index.tsx
@@ -0,0 +1,17 @@
+import { useTranslation } from "react-i18next";
+
+import styles from "./styles.module.css";
+
+export const CronTableHeader = () => {
+    const { t } = useTranslation();
+
+    return (
+        <>
+            <div className={`TableHeader ${styles.CronTableHeader}`}>
+                <div className="RowChunk">{t("ScheduledTasks.runnable")}</div>
+                <div className="RowChunk">{t("ScheduledTasks.expression")}</div>
+                <div className={`RowChunk ${styles.Status}`}>{t("ScheduledTasks.status")}</div>
+            </div>
+        </>
+    );
+};
