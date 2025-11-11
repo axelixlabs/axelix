7fc69abda6ab3354514e30721ce373bb5d6784e2
Ashot Sargsyan <135041956+AshotSargsyan1@users.noreply.github.com>
2025-11-11 12:36:01 +0400

    GH-315 Displayed the bean source in the UI (#322)

---
+++ front-end/src/pages/Beans/BeanAccordionChildren/BeanSource/index.tsx
@@ -0,0 +1,89 @@
+import { Tree, type TreeDataNode } from "antd";
+import { useTranslation } from "react-i18next";
+
+import { EBeanOrigin, type IBeanSource } from "models";
+
+import sharedStyles from "../styles.module.css";
+
+import styles from "./styles.module.css";
+
+interface IProps {
+    beanSource: IBeanSource;
+}
+
+export const BeanSource = ({ beanSource }: IProps) => {
+    const { t } = useTranslation();
+
+    const statelessBeanSource =
+        beanSource.origin === EBeanOrigin.UNKNOWN || beanSource.origin === EBeanOrigin.COMPONENT_ANNOTATION;
+
+    const translatedTitle = t(`Beans.beanSource.${beanSource.origin}`);
+
+    const resolveTreeChildren = (): TreeDataNode[] | undefined => {
+        if (beanSource.origin === EBeanOrigin.BEAN_METHOD) {
+            return [
+                {
+                    title: translatedTitle,
+                    key: beanSource.origin,
+                    children: [
+                        {
+                            title: (
+                                <div className={styles.BeanTreeItem}>
+                                    <div className={styles.BeanTreeLabel}>
+                                        {t("Beans.beanSource.titles.beanMethod")}:
+                                    </div>
+                                    <div className={styles.BeanTreeValue}>{beanSource.methodName}</div>
+                                </div>
+                            ),
+                            key: beanSource.methodName!,
+                        },
+                        {
+                            title: (
+                                <div className={styles.BeanTreeItem}>
+                                    <div className={styles.BeanTreeLabel}>
+                                        {t("Beans.beanSource.titles.enclosingClass")}:
+                                    </div>
+                                    <div className={styles.BeanTreeValue}>{beanSource.enclosingClassName}</div>
+                                </div>
+                            ),
+                            key: beanSource.enclosingClassName!,
+                        },
+                    ],
+                },
+            ];
+        }
+
+        if (beanSource.origin === EBeanOrigin.FACTORY_BEAN) {
+            return [
+                {
+                    title: translatedTitle,
+                    key: beanSource.origin,
+                    children: [
+                        {
+                            title: (
+                                <div className={styles.BeanTreeItem}>
+                                    <div className={styles.BeanTreeLabel}>
+                                        {t("Beans.beanSource.titles.factoryBeanName")}:
+                                    </div>
+                                    <div className={styles.BeanTreeValue}>{beanSource.factoryBeanName}</div>
+                                </div>
+                            ),
+                            key: beanSource.factoryBeanName!,
+                        },
+                    ],
+                },
+            ];
+        }
+    };
+
+    return (
+        <>
+            <div className={sharedStyles.AccordionBodyChunkTitle}>{t(`Beans.beanSource.titles.main`)}:</div>
+            {statelessBeanSource ? (
+                translatedTitle
+            ) : (
+                <Tree expandAction="click" showLine treeData={resolveTreeChildren()} className={styles.Tree} />
+            )}
+        </>
+    );
+};
