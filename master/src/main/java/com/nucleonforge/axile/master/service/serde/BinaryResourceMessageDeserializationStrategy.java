cbe93c774031abdc119a2d257cdca59d09742715
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-11-12 23:05:51 +0300

    GH-331: Polishing
    Signed-off-by: Mikhail Polivakha <mikhailpolivakha@email.com>

--- master/src/main/java/com/nucleonforge/axile/master/service/serde/BinaryResourceMessageDeserializationStrategy.java
+++ master/src/main/java/com/nucleonforge/axile/master/service/serde/BinaryResourceMessageDeserializationStrategy.java
@@ -6,7 +6,8 @@ import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 
 /**
- * {@link MessageDeserializationStrategy} based on {@link Resource}.
+ * {@link MessageDeserializationStrategy} for the deserialization of the {@code byte[]}
+ * into the {@link Resource}.
  *
  * @since 12.11.2025
  * @author Nikita Kirillov
@@ -17,6 +18,7 @@ public abstract class BinaryResourceMessageDeserializationStrategy implements Me
     public @NonNull Resource deserialize(byte @NonNull [] binary) throws DeserializationException {
         try {
             return new ByteArrayResource(binary) {
+
                 @Override
                 public String getFilename() {
                     return filename();
@@ -27,5 +29,8 @@ public abstract class BinaryResourceMessageDeserializationStrategy implements Me
         }
     }
 
+    /**
+     * @return the file name of the deserialized resource.
+     */
     protected abstract String filename();
 }
--- master/src/main/java/com/nucleonforge/axile/master/service/serde/HeapDumpMessageDeserializationStrategy.java
+++ master/src/main/java/com/nucleonforge/axile/master/service/serde/HeapDumpMessageDeserializationStrategy.java
@@ -11,6 +11,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class HeapDumpMessageDeserializationStrategy extends BinaryResourceMessageDeserializationStrategy {
 
+    /**
+     * This filename extension is valid only for HotSpot heapdump format.
+     * <p>
+     * Although, in 99% of cases hprof is the format of the actual binary
+     * deserialized file, it might still be that someone is using OpenJ9 for
+     * instance or anything, and head dump format would differ from hprof.
+     */
     @Override
     protected String filename() {
         return "heapdump.hprof";
