a5aa159b69337e25b56bb5a52050c6906b364524
Mikhail Polivakha <mikhailpolivakha@email.com>
2025-11-13 15:38:15 +0300

    Polishing: introduced grouping for the metrics

--- master/src/main/java/com/nucleonforge/axile/master/api/response/metrics/MetricsListResponse.java
+++ master/src/main/java/com/nucleonforge/axile/master/api/response/metrics/MetricsListResponse.java
@@ -3,9 +3,17 @@ package com.nucleonforge.axile.master.api.response.metrics;
 import java.util.List;
 
 /**
- * Response to the {@link MetricsApi#MAIN}.
+ * Response to the {@link com.nucleonforge.axile.master.api.ApiPaths.MetricsApi#MAIN}.
  *
- * @param names list of metric names.
  * @author Mikhail Polivakha
  */
-public record MetricsListResponse(List<String> names) {}
+public record MetricsListResponse(List<MetricsGroup> metricsGroups) {
+
+    /**
+     * Metrics Group.
+     *
+     * @param groupName the name of the group to which the {@link #metrics} belong to.
+     * @param metrics the names of the metrics inside teh given group.
+     */
+    public record MetricsGroup(String groupName, List<String> metrics) {}
+}
