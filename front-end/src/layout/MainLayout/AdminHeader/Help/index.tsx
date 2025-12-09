df1844f038ea454d61e3e7e461c1d659871a1d8d
Ashot <ashotsargsyan527@gmail.com>
2025-12-09 12:03:45 +0400

    GH-454 Implemented Help navigation section on the UI

---
+++ front-end/src/layout/MainLayout/AdminHeader/Help/index.tsx
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2025-present, Nucleon Forge Software.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import { BookOutlined, CommentOutlined, DownOutlined } from "@ant-design/icons";
+
+import { Dropdown, type MenuProps, Space } from "antd";
+import AboutIcon from "assets/icons/info-black.svg";
+import { useTranslation } from "react-i18next";
+
+import styles from "./styles.module.css";
+
+export const Help = () => {
+    const { t } = useTranslation();
+
+    const items: MenuProps["items"] = [
+        {
+            key: "1",
+            type: "group",
+            label: "Axelix V1.0.10",
+        },
+        {
+            type: "divider",
+        },
+        {
+            key: "2",
+            icon: <BookOutlined className={styles.CommonIcon} />,
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="#">
+                    {t("Header.Help.documentation")}
+                </a>
+            ),
+        },
+        {
+            key: "3",
+            // TODO: Replace this via svgr in future and delete the info-black.svg icon
+            icon: <img src={AboutIcon} alt="About icon" />,
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="#">
+                    {t("Header.Help.about")}
+                </a>
+            ),
+        },
+        {
+            key: "4",
+            icon: <CommentOutlined className={styles.CommonIcon} />,
+            label: (
+                <a target="_blank" rel="noopener noreferrer" href="#">
+                    {t("Header.Help.feedback")}
+                </a>
+            ),
+        },
+    ];
+
+    return (
+        <Dropdown menu={{ items }} className={styles.MainWrapper}>
+            <a onClick={(e) => e.preventDefault()} className={styles.MenuWrapper}>
+                <Space>
+                    {t("Header.Help.title")}
+                    <DownOutlined />
+                </Space>
+            </a>
+        </Dropdown>
+    );
+};
