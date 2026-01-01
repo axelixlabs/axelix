8c6a2c5073d7837cd20513d1f96d15c3afdd5da1
Mikhail Polivakha <mikhailpolivakha@email.com>
2026-01-01 12:49:53 +0300

    Polishing: adopted a new coloring strategy for dashboard pie charts

--- front-end/src/utils/dashboard.ts
+++ front-end/src/utils/dashboard.ts
@@ -20,3 +20,36 @@ export const HEALTH_STATUSES_COLORS: Record<EInstanceStatus, string> = {
     [EInstanceStatus.DOWN]: "#ff000a",
     [EInstanceStatus.UNKNOWN]: "#838383",
 };
+
+const PIE_CHARTS_COLORS_PALETTE = ["#0366a6", "#9061aa", "#d68026", "#ffff00", "#006600"];
+
+/**
+ * Cache that stores the mapping between dashboard software component versions and their colors on the pie charts.
+ */
+const colorsCache: Map<string, Map<string, string>> = new Map();
+
+const colorIndexesCache: Map<string, number> = new Map();
+
+/**
+ * Function that accepts the name of the software component version and returns the hex color for it on the pie chart.
+ */
+export const pickColor = (componentName: string, componentVersion: string) => {
+    let componentColors = colorsCache.get(componentName);
+
+    if (!componentColors) {
+        componentColors = new Map();
+        colorsCache.set(componentName, componentColors);
+    }
+
+    let colorForVersion = componentColors.get(componentVersion);
+
+    if (!colorForVersion) {
+        const colorIndex = colorIndexesCache.get(componentName) ?? 0;
+        colorIndexesCache.set(componentName, colorIndex + 1);
+
+        colorForVersion = PIE_CHARTS_COLORS_PALETTE[colorIndex];
+        componentColors.set(componentVersion, colorForVersion);
+    }
+
+    return colorForVersion;
+};
