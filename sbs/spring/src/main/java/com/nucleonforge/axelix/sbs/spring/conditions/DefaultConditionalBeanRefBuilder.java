addfb059a90b3f91d939e53ef6b46dab4122860d
sergeycherkasovv <iamcherkasov.job@gmail.com>
2026-01-12 17:11:44 +0700

    GH-507: proxy detection logic has been moved to the common module (#507)

--- sbs/spring/src/main/java/com/nucleonforge/axelix/sbs/spring/conditions/DefaultConditionalBeanRefBuilder.java
+++ sbs/spring/src/main/java/com/nucleonforge/axelix/sbs/spring/conditions/DefaultConditionalBeanRefBuilder.java
@@ -15,11 +15,11 @@
  */
 package com.nucleonforge.axelix.sbs.spring.conditions;
 
-import java.lang.reflect.Proxy;
-
 import org.jspecify.annotations.Nullable;
 
 import org.springframework.util.ClassUtils;
+
+import com.nucleonforge.axelix.common.utils.ProxyUtils;
 
 /**
  * Default implementation {@link ConditionalBeanRefBuilder}.
@@ -31,7 +31,7 @@ public class DefaultConditionalBeanRefBuilder implements ConditionalBeanRefBuild
 
     public String buildBeanRefInternal(Class<?> beanClass, @Nullable String beanFactoryMethodName) {
 
-        Class<?> userClass = resolveUserClass(beanClass);
+        Class<?> userClass = ProxyUtils.resolveUserClass(beanClass);
 
         // By calling this method spring-boot replaces the nested class '$' with package separator - the dot.
         // https://github.com/spring-projects/spring-boot/blob/main/module/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/condition/ConditionsReportEndpoint.java#L128
@@ -44,17 +44,5 @@ public class DefaultConditionalBeanRefBuilder implements ConditionalBeanRefBuild
         }
 
         return result.toString();
-    }
-
-    // TODO:
-    //  We have this logic  of determining the end user class spread
-    //  across the project, especially in beans endpoint. Need to unify it.
-    private static Class<?> resolveUserClass(Class<?> beanClass) {
-        Class<?> userClass = ClassUtils.getUserClass(beanClass);
-
-        if (userClass == beanClass && Proxy.isProxyClass(userClass)) {
-            userClass = userClass.getInterfaces()[0];
-        }
-        return userClass;
     }
 }
