304d6a84f675fee6a0f3b54abd8e5364de4118e7
Ashot Sargsyan <135041956+AshotSargsyan1@users.noreply.github.com>
2026-02-06 18:19:21 +0400

    GH-671: Introduced new design for wallboard cards
    * GH-671 Draft of the Wallboard new card Design
    
    * GH-671 Implemented new Wallboard card design

--- front-end/src/pages/Wallboard/WallboardCard/index.tsx
+++ front-end/src/pages/Wallboard/WallboardCard/index.tsx
@@ -15,9 +15,11 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+import { Badge } from "antd";
+import QuestionIcon from "assets/icons/question.svg?react";
 import { Link } from "react-router-dom";
 
-import type { IInstanceCard } from "models";
+import { EInstanceStatus, type IInstanceCard } from "models";
 
 import styles from "./styles.module.css";
 
@@ -29,16 +31,52 @@ interface IProps {
 }
 
 export const WallboardCard = ({ data }: IProps) => {
+    const isUp = data.status === EInstanceStatus.UP;
+    const isUnknown = data.status === EInstanceStatus.UNKNOWN;
+    const isDown = data.status === EInstanceStatus.DOWN;
+
     return (
-        <Link to={`/instance/${data.instanceId}/details`} className={`${styles.Card} ${styles[`Card${data.status}`]}`}>
-            <div className={`${styles.CardHeader} ${styles[`CardHeader${data.status}`]}`}>{data.name}</div>
-            <div className={styles.CardBody}>
-                <div>Version: {data.serviceVersion}</div>
-                <div>Spring Boot: {data.springBootVersion}</div>
-                <div>Java: {data.javaVersion}</div>
-                <div className={styles.HashAndTimeWrapper}>
-                    <span>Commit: {data.commitShaShort}</span>
-                    <span>{data.deployedFor}</span>
+        <Link
+            to={`/instance/${data.instanceId}/details`}
+            className={`${styles.Card} ${!isUp ? styles[data.status] : ""}`}
+        >
+            <div className={styles.Header}>
+                {/* TODO: Here we need to show an ellipsis with a tooltip */}
+                <div className={styles.Title}>{data.name}</div>
+                {!isUnknown ? (
+                    <Badge
+                        status={isDown ? "processing" : undefined}
+                        color={isDown ? "#ff000a" : "#00ab55"}
+                        styles={{
+                            indicator: {
+                                width: "8px",
+                                height: "8px",
+                            },
+                        }}
+                    />
+                ) : (
+                    <QuestionIcon className={styles.QuestionIcon} style={{ flexShrink: "0" }} />
+                )}
+            </div>
+            <div className={styles.Body}>
+                <div className={styles.BodyChunk}>
+                    <div className={styles.Label}>Version</div>
+                    <div className={styles.Value}>{data.serviceVersion}</div>
+                </div>
+
+                <div className={styles.BodyChunk}>
+                    <div className={styles.Label}>Spring Boot</div>
+                    <div className={styles.Value}>{data.springBootVersion}</div>
+                </div>
+
+                <div className={styles.BodyChunk}>
+                    <div className={styles.Label}>Java</div>
+                    <div className={styles.Value}>{data.javaVersion}</div>
+                </div>
+
+                <div className={styles.Footer}>
+                    <div className={styles.Label}>Commit: {data.commitShaShort}</div>
+                    <div className={styles.Value}>{data.deployedFor}</div>
                 </div>
             </div>
         </Link>
