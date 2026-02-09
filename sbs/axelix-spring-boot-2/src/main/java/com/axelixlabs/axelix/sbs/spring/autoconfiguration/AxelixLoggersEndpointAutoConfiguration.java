bbd8499251ab61469fc2ddb428ff1f0b0853aff9
sergeycherkasovv <iamcherkasov.job@gmail.com>
2026-02-09 15:40:04 +0300

    GH-644: Ported Loggers endpoint
    Co-authored-by: mipo256 <mikhailpolivakha@email.com>

---
+++ sbs/axelix-spring-boot-2/src/main/java/com/axelixlabs/axelix/sbs/spring/autoconfiguration/AxelixLoggersEndpointAutoConfiguration.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2025-2026 Axelix Labs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.axelixlabs.axelix.sbs.spring.autoconfiguration;
+
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.actuate.logging.LoggersEndpoint;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.logging.LoggingApplicationListener;
+import org.springframework.boot.logging.LoggerGroups;
+import org.springframework.boot.logging.LoggingSystem;
+import org.springframework.context.annotation.Bean;
+
+import com.axelixlabs.axelix.sbs.spring.core.loggers.AxelixLoggersEndpoint;
+
+/**
+ * Auto-configuration for the {@link AxelixLoggersEndpoint}.
+ *
+ * @author Sergey Cherkasov
+ */
+@AutoConfiguration
+public class AxelixLoggersEndpointAutoConfiguration {
+
+    /**
+     * {@link LoggingSystem} and {@link LoggerGroups} beans are registered dynamically in {@link LoggingApplicationListener}.
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public AxelixLoggersEndpoint axelixLoggersEndpoint(
+            LoggingSystem loggingSystem, ObjectProvider<LoggerGroups> loggerGroups) {
+        return new AxelixLoggersEndpoint(
+                new LoggersEndpoint(loggingSystem, loggerGroups.getIfAvailable(LoggerGroups::new)));
+    }
+}
