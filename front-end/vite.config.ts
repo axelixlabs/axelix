1a7039dd7501459e87eebcefe3138605014bb749
Ashot <ashotsargsyan527@gmail.com>
2026-02-26 12:53:07 +0400

    Change

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -19,13 +19,11 @@ import react from "@vitejs/plugin-react";
 
 import * as path from "path";
 import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 
 export default defineConfig(() => {
-    // we have to load the .env file manually here since vite interprets the .env after the config getting loaded
-    const env = loadEnv("", process.cwd(), "");
-    const apiTarget = env.VITE_LOCAL_API_URL ?? "http://localhost:8080";
+    const apiTarget = import.meta.env.VITE_LOCAL_API_URL ?? "http://localhost:8080";
 
     return {
         plugins: [
