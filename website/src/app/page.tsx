fbc1f6037c6ea6f252e48225a86e5d5893c6e062
Ashot <ashotsargsyan527@gmail.com>
2026-03-05 18:55:43 +0400

    GH-794 Changes

--- website/src/app/page.tsx
+++ website/src/app/page.tsx
@@ -3,6 +3,7 @@ import { FirstSection, WhatCanDo, Installation, Zigzag, FAQ } from "@/components
 export default function Home() {
   return (
     <>
+        ТЕст
       <FirstSection />
       <Zigzag />
       <Installation />
