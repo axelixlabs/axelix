9a665404c5d06f32c76793a9b03cefc29e55b02d
Ashot <ashotsargsyan527@gmail.com>
2026-03-05 15:03:32 +0400

    GH-794 Changes

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -21,7 +21,7 @@ const config: Config = {
   url: 'https://axelix.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   onBrokenLinks: 'throw',
 
