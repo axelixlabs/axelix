39633534d1e315cd0adaecb2df5eec2fe85fb916
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-03-18 19:55:18 +0300

    GH-769: Significantly simplified the SettingsApi test

--- master/src/test/java/com/axelixlabs/axelix/master/api/SettingsApiTest.java
+++ master/src/test/java/com/axelixlabs/axelix/master/api/SettingsApiTest.java
@@ -18,45 +18,35 @@
 package com.axelixlabs.axelix.master.api;
 
 import java.io.IOException;
-import java.util.Objects;
 
-import okhttp3.mockwebserver.Dispatcher;
-import okhttp3.mockwebserver.MockResponse;
+import com.axelixlabs.axelix.master.api.external.endpoint.SettingsApi;
+import com.axelixlabs.axelix.master.service.auth.oauth.OidcMetadataProvider;
 import okhttp3.mockwebserver.MockWebServer;
-import okhttp3.mockwebserver.RecordedRequest;
-import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.http.client.ClientHttpRequestFactorySettings;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.util.ReflectionTestUtils;
-
-import com.axelixlabs.axelix.common.utils.Lazy;
-import com.axelixlabs.axelix.master.api.external.endpoint.SettingsApi;
-import com.axelixlabs.axelix.master.service.auth.oauth.OidcMetadataProvider;
-import com.axelixlabs.axelix.master.service.auth.oauth.OidcMetadataProvider.OidcMetadata;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 import static net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 /**
  * Integration tests for {@link SettingsApi}.
  *
  * @since 06.03.2026
  * @author Nikita Kirillov
+ * @author Mikhail Polivakha
  */
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class SettingsApiTest {
 
     private static MockWebServer mockWebServer;
@@ -82,20 +72,14 @@ class SettingsApiTest {
     @Nested
     class WhenStaticAdminEnabled {
 
-        @LocalServerPort
-        private int port;
-
+        // The TestRestTemplateBuilder is intentionally not used here, since we do not require any auth to access settings API.
+        @Autowired
         private TestRestTemplate restTemplate;
-
-        @BeforeEach
-        void prepare() {
-            restTemplate = new TestRestTemplate().withRedirects(ClientHttpRequestFactorySettings.Redirects.DONT_FOLLOW);
-        }
 
         @Test
         void shouldReturnStaticAdminSettings() {
-            ResponseEntity<String> response =
-                    restTemplate.getForEntity("http://localhost:" + port + "/api/external/settings/auth", String.class);
+
+            ResponseEntity<String> response = restTemplate.getForEntity("/api/external/settings/auth", String.class);
 
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
             // language=json
@@ -124,9 +108,7 @@ class SettingsApiTest {
     @Nested
     class WhenOAuth2Enabled {
 
-        private final String baseUrl = mockWebServer.url("").toString();
-
-        private final String EXPECTED_JSON =
+        private static final String EXPECTED_JSON =
                 // language=json
                 """
             {
@@ -135,63 +117,28 @@ class SettingsApiTest {
                   "clientId": "test-client",
                   "redirectUri": "http://localhost:3000/api/external/oauth2/callback",
                   "scope": "openid",
-                  "authorizationEndpoint": "%srealms/axelix/openid-connect/auth",
+                  "authorizationEndpoint": "https://example.external.com/realms/axelix/openid-connect/auth",
                   "type": "oauth2"
                 }
               ]
             }
-            """.formatted(baseUrl);
+            """;
 
-        @LocalServerPort
-        private int port;
-
+        // The TestRestTemplateBuilder is intentionally not used here, since we do not require any auth to access settings API.
+        @Autowired
         private TestRestTemplate restTemplate;
 
-        @Autowired
+        @MockitoBean
         private OidcMetadataProvider oidcMetadataProvider;
 
         @BeforeEach
         void prepare() {
-            restTemplate = new TestRestTemplate().withRedirects(ClientHttpRequestFactorySettings.Redirects.DONT_FOLLOW);
-
-            String baseUrl = mockWebServer.url("").toString();
-
-            // override issuerUri after start mockWebServer
-            ReflectionTestUtils.setField(
-                    oidcMetadataProvider,
-                    "lazyOidcMetadata",
-                    Lazy.resolved(new OidcMetadata(
-                            baseUrl, baseUrl + "token", baseUrl + "realms/axelix/openid-connect/auth")));
-
-            mockWebServer.setDispatcher(new Dispatcher() {
-                @Override
-                public @NonNull MockResponse dispatch(@NonNull RecordedRequest request) {
-                    String path = request.getPath();
-                    assert path != null;
-                    if (path.equals("/.well-known/openid-configuration")
-                            && Objects.equals(request.getMethod(), "GET")) {
-                        return new MockResponse()
-                                .setBody(
-                                        // language=json
-                                        """
-                                        {
-                                          "issuer": "%s",
-                                          "jwks_uri": "%scerts",
-                                          "token_endpoint": "%stoken",
-                                          "authorization_endpoint": "%srealms/axelix/protocol/openid-connect/auth"
-                                        }
-                                        """.formatted(baseUrl, baseUrl, baseUrl, baseUrl))
-                                .addHeader("Content-Type", APPLICATION_JSON_VALUE);
-                    }
-                    return new MockResponse().setResponseCode(404);
-                }
-            });
+            Mockito.when(oidcMetadataProvider.getAuthorizationEndpoint()).thenReturn("https://example.external.com/realms/axelix/openid-connect/auth");
         }
 
         @Test
         void shouldReturnOAuth2Settings() {
-            ResponseEntity<String> response =
-                    restTemplate.getForEntity("http://localhost:" + port + "/api/external/settings/auth", String.class);
+            ResponseEntity<String> response = restTemplate.getForEntity("/api/external/settings/auth", String.class);
 
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
             assertThatJson(response.getBody()).isEqualTo(EXPECTED_JSON);
@@ -213,9 +160,7 @@ class SettingsApiTest {
     @Nested
     class WhenStaticAdminAndOAuth2Enabled {
 
-        private final String baseUrl = mockWebServer.url("").toString();
-
-        private final String EXPECTED_JSON =
+        private static final String EXPECTED_JSON =
                 // language=json
                 """
                 {
@@ -224,7 +169,7 @@ class SettingsApiTest {
                       "clientId": "test-client",
                       "redirectUri": "http://localhost:3000/api/external/oauth2/callback",
                       "scope": "openid",
-                      "authorizationEndpoint": "%srealms/axelix/openid-connect/auth",
+                      "authorizationEndpoint": "https://example.external.com/realms/axelix/openid-connect/auth",
                       "type": "oauth2"
                     },
                     {
@@ -232,57 +177,22 @@ class SettingsApiTest {
                     }
                   ]
                 }
-                """.formatted(baseUrl);
-
-        @LocalServerPort
-        private int port;
-
-        private TestRestTemplate restTemplate;
+                """;
 
         @Autowired
+        private TestRestTemplate restTemplate;
+
+        @MockitoBean
         private OidcMetadataProvider oidcMetadataProvider;
 
         @BeforeEach
         void prepare() {
-            restTemplate = new TestRestTemplate().withRedirects(ClientHttpRequestFactorySettings.Redirects.DONT_FOLLOW);
-
-            String baseUrl = mockWebServer.url("").toString();
-
-            // override issuerUri after start mockWebServer
-            ReflectionTestUtils.setField(
-                    oidcMetadataProvider,
-                    "lazyOidcMetadata",
-                    Lazy.resolved(new OidcMetadata(
-                            baseUrl, baseUrl + "token", baseUrl + "realms/axelix/openid-connect/auth")));
-
-            mockWebServer.setDispatcher(new Dispatcher() {
-                @Override
-                public @NonNull MockResponse dispatch(@NonNull RecordedRequest request) {
-                    String path = request.getPath();
-                    assert path != null;
-                    if (path.equals("/.well-known/openid-configuration")
-                            && Objects.equals(request.getMethod(), "GET")) {
-                        return new MockResponse()
-                                // language=json
-                                .setBody("""
-                                    {
-                                      "issuer": "%s",
-                                      "jwks_uri": "%scerts",
-                                      "token_endpoint": "%stoken",
-                                      "authorization_endpoint": "%srealms/axelix/openid-connect/auth"
-                                    }
-                                    """.formatted(baseUrl, baseUrl, baseUrl, baseUrl))
-                                .addHeader("Content-Type", APPLICATION_JSON_VALUE);
-                    }
-                    return new MockResponse().setResponseCode(404);
-                }
-            });
+            Mockito.when(oidcMetadataProvider.getAuthorizationEndpoint()).thenReturn("https://example.external.com/realms/axelix/openid-connect/auth");
         }
 
         @Test
         void shouldReturnBothProviders() {
-            ResponseEntity<String> response =
-                    restTemplate.getForEntity("http://localhost:" + port + "/api/external/settings/auth", String.class);
+            ResponseEntity<String> response = restTemplate.getForEntity("/api/external/settings/auth", String.class);
 
             assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
             assertThatJson(response.getBody()).isEqualTo(EXPECTED_JSON);
