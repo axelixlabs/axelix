2bc9985b9359ed3ea8cd1e345c6b4164febdf18a
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-03-20 15:20:32 +0300

    Polishing

--- sbs/axelix-spring-boot-3/src/main/java/com/axelixlabs/axelix/sbs/spring/autoconfiguration/PropertyManagementAutoConfiguration.java
+++ sbs/axelix-spring-boot-3/src/main/java/com/axelixlabs/axelix/sbs/spring/autoconfiguration/PropertyManagementAutoConfiguration.java
@@ -27,28 +27,12 @@ import com.axelixlabs.axelix.sbs.spring.core.env.DefaultPropertyNameNormalizer;
 import com.axelixlabs.axelix.sbs.spring.core.env.PropertyNameNormalizer;
 import com.axelixlabs.axelix.sbs.spring.core.properties.ContextReloadingPropertyMutator;
 import com.axelixlabs.axelix.sbs.spring.core.properties.DefaultPropertyNameDiscoverer;
-import com.axelixlabs.axelix.sbs.spring.core.properties.DefaultPropertySourceDescriber;
 import com.axelixlabs.axelix.sbs.spring.core.properties.PropertyManagementEndpoint;
 import com.axelixlabs.axelix.sbs.spring.core.properties.PropertyMutator;
 import com.axelixlabs.axelix.sbs.spring.core.properties.PropertyNameDiscoverer;
-import com.axelixlabs.axelix.sbs.spring.core.properties.PropertySourceDescriber;
 
 /**
  * Auto-configuration for property management operations via Spring Boot Actuator.
- *
- * <p>This configuration provides beans to discover, describe, and mutate application properties at runtime,
- * as well as an actuator endpoint to expose these capabilities.</p>
- *
- * <p>Beans registered by this auto-configuration (if missing) include:</p>
- * <ul>
- *   <li>{@link PropertyMutator} — responsible for updating property values and triggering application context restarts.</li>
- *   <li>{@link PropertyNameDiscoverer} — responsible for locating properties name within the Spring Environment.</li>
- *   <li>{@link PropertyManagementEndpoint} — actuator endpoint exposing property management operations.</li>
- *   <li>{@link PropertySourceDescriber} - Provides metadata descriptions of property sources.</li>
- * </ul>
- *
- * <p>This auto-configuration is applied after {@link ContextRestarterAutoConfiguration}
- * to ensure context restart capabilities are available when mutating properties.</p>
  *
  * @since 10.07.2025
  * @author Nikita Kirillov
@@ -80,11 +64,5 @@ public class PropertyManagementAutoConfiguration {
     public PropertyManagementEndpoint propertyManagementEndpoint(
             PropertyMutator propertyMutator, PropertyNameDiscoverer propertyNameDiscoverer) {
         return new PropertyManagementEndpoint(propertyMutator, propertyNameDiscoverer);
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public PropertySourceDescriber propertySourceDescriber() {
-        return new DefaultPropertySourceDescriber();
     }
 }
