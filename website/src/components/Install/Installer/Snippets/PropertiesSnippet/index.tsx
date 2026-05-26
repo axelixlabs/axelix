94516b09b6cd80e371c680e0514c2e025c90cc6f
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-05-26 12:42:59 +0300

    Adjusted website 3rd insllation step's properties

--- website/src/components/Install/Installer/Snippets/PropertiesSnippet/index.tsx
+++ website/src/components/Install/Installer/Snippets/PropertiesSnippet/index.tsx
@@ -7,23 +7,31 @@ interface IProps {
 export const PropertiesSnippet = ({ refEl }: IProps) => {
     return (
         <pre
-            className={`${styles.Snippet} ${styles.Active}`}
+            className={styles.Snippet}
             ref={(el) => {
                 refEl.current = el;
             }}
         >
             <code>
                 <span className={styles.Line}>
-                    <span className={styles.At}>axelix.master.url</span>=
-                    <span className={styles.St}>http://localhost:8080</span>
+                    <span className={styles.At}>axelix.sbs.auth.jwt.algorithm</span>=
+                    <span className={styles.St}>HMAC512</span>
                 </span>
                 <span className={styles.Line}>
-                    <span className={styles.At}>axelix.auth.jwt.algorithm</span>=
-                    <span className={styles.St}>HMAC256</span>
+                    <span className={styles.At}>axelix.sbs.auth.jwt.signing-key</span>=
+                    <span className={styles.St}>8DrZJSOJ8vkbxdjUB3sSsyeiG4Xidf1sDNmJq1Slkkn</span>
                 </span>
                 <span className={styles.Line}>
-                    <span className={styles.At}>axelix.auth.jwt.signing-key</span>=
-                    <span className={styles.Kw}>{"${AXELIX_TOKEN}"}</span>
+                    <span className={styles.At}>axelix.sbs.discovery.instance-name</span>=
+                    <span className={styles.St}>my-app</span>
+                </span>
+                <span className={styles.Line}>
+                    <span className={styles.At}>axelix.sbs.discovery.instance-url</span>=
+                    <span className={styles.St}>https://my-app.com/actuator</span>
+                </span>
+                <span className={styles.Line}>
+                    <span className={styles.At}>axelix.sbs.discovery.master-url</span>=
+                    <span className={styles.St}>https://axelix-master.com/api/internal/service/register</span>
                 </span>
             </code>
         </pre>
