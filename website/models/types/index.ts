92284dc999486a66d43c090c491677ff796e8910
Ashot <ashotsargsyan527@gmail.com>
2026-06-03 10:36:03 +0400

    GH-1137 Added license headers

--- website/models/types/index.ts
+++ website/models/types/index.ts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2025-2026 Axelix Labs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 export type Method = "docker" | "compose" | "k8s" | "bare";
 export type SbVariant = "sb2" | "sb3" | "sb4";
 export type CfgVariant = "yaml" | "properties";
