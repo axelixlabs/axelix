9c7bbcb43087b8f2aefeb47f64bf6aeccc733bfa
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-07-17 23:25:07 +0300

    Added TransactionStatsTest

---
+++ sbs/starter-domain/src/test/java/com/axelixlabs/axelix/sbs/spring/core/persistence/transaction/TransactionStatsTest.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (C) 2025-2026 Axelix Labs
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.axelixlabs.axelix.sbs.spring.core.persistence.transaction;
+
+import java.time.Instant;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import com.axelixlabs.axelix.sbs.spring.core.persistence.SimpleSqlQueryRecord;
+import com.axelixlabs.axelix.sbs.spring.core.persistence.hibernate.LazyLoadingTarget;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {@link TransactionStats}.
+ *
+ * @author Mikhail Polivakha
+ */
+class TransactionStatsTest {
+
+    @Nested
+    class InMemoryPaginationAggregation {
+
+        @Test
+        void shouldExtractTableNameAfterFromClause() {
+            // given.
+            TransactionStats subject = new TransactionStats();
+            TransactionExecutionProfile profile =
+                    profileWith(inMemoryPaginated("select owner0_.id from owner owner0_ order by owner0_.id"));
+
+            // when.
+            subject.put(profile);
+
+            // then.
+            assertThat(subject.getInMemoryPaginatedEntities()).containsEntry("owner", 1);
+        }
+
+        @Test
+        void shouldExtractTableNameWhenFromIsUpperCase() {
+            // given.
+            TransactionStats subject = new TransactionStats();
+            TransactionExecutionProfile profile = profileWith(inMemoryPaginated("SELECT id FROM pet p WHERE p.id = 1"));
+
+            // when.
+            subject.put(profile);
+
+            // then.
+            assertThat(subject.getInMemoryPaginatedEntities()).containsEntry("pet", 1);
+        }
+
+        @Test
+        void shouldSkipInMemoryPaginatedQueryWithoutFromClause() {
+            // given.
+            TransactionStats subject = new TransactionStats();
+            TransactionExecutionProfile profile = profileWith(inMemoryPaginated("select 1"));
+
+            // when.
+            subject.put(profile);
+
+            // then.
+            assertThat(subject.getInMemoryPaginatedEntities()).isEmpty();
+        }
+
+        @Test
+        void shouldKeepMaxCountAcrossTransactionsForSameTarget() {
+            // given.
+            TransactionStats subject = new TransactionStats();
+
+            // when.
+            subject.put(profileWith(
+                    inMemoryPaginated("select * from owner o"), inMemoryPaginated("select * from owner o2")));
+            subject.put(profileWith(inMemoryPaginated("select * from owner o")));
+
+            // then.
+            assertThat(subject.getInMemoryPaginatedEntities()).containsEntry("owner", 2);
+        }
+    }
+
+    @Nested
+    class NPlusOneAggregation {
+
+        @Test
+        void shouldAggregateIdenticalLazyLoadingTargets() {
+            // given.
+            TransactionStats subject = new TransactionStats();
+            // new LazyLoadingTarget objects here are used on purpose so that we want to check for
+            // the LazyLoadingTarget equal/hashcode contract.
+            LazyLoadingTarget pets = new LazyLoadingTarget(SampleEntity.class, "pets");
+
+            TransactionExecutionProfile profile = new TransactionExecutionProfile(Instant.now());
+            profile.recordQuery(simple("select * from owner"));
+            profile.recordLazyLoading(new LazyLoadingTarget(SampleEntity.class, "pets"));
+            profile.recordQuery(simple("select * from pet where owner_id = 1"));
+            profile.recordLazyLoading(new LazyLoadingTarget(SampleEntity.class, "pets"));
+            profile.complete();
+
+            // when.
+            subject.put(profile);
+
+            // then.
+            assertThat(subject.getNPlusOneOccasions()).containsEntry(pets, 2);
+            assertThat(subject.getInMemoryPaginatedEntities()).isEmpty();
+        }
+    }
+
+    private static TransactionExecutionProfile profileWith(SimpleSqlQueryRecord... queries) {
+        TransactionExecutionProfile profile = new TransactionExecutionProfile(Instant.now());
+        for (SimpleSqlQueryRecord query : queries) {
+            profile.recordQuery(query);
+        }
+        return profile.complete();
+    }
+
+    private static SimpleSqlQueryRecord inMemoryPaginated(String sql) {
+        return new SimpleSqlQueryRecord(sql, 5L, 1_000L, true);
+    }
+
+    private static SimpleSqlQueryRecord simple(String sql) {
+        return new SimpleSqlQueryRecord(sql, 5L, 1_000L, false);
+    }
+
+    private static class SampleEntity {}
+}
