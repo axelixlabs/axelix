07448aa823aebe6021085f3b946a8724be7a0b47
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-08-03 16:07:14 +0300

    Polished: adopted docs in accordance with ADR

--- docs/config/preset-options.ts
+++ docs/config/preset-options.ts
@@ -11,6 +11,14 @@ export const presetOptions: Preset.Options = {
     // Remove this to remove the "edit this page" links.
 
     editUrl: 'https://github.com/axelixlabs/axelix/edit/master/docs/',
+    // Docs are maintained per major release. Until a new major (2.x) branches
+    // off, the working docs ARE the 1.x docs, so we label the current version
+    // "1.x" and defer freezing a snapshot until 2.0 is cut.
+    versions: {
+      current: {
+        label: '1.x',
+      },
+    },
   },
   blog: false,
   theme: {
