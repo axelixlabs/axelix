fdf951eabee16b549ebf8321e2b9446689aa925a
nikita <kirilloffnikita1@gmail.com>
2026-08-28 02:08:47 +0500

    GH-1548 Generate THIRD-PARTY-NOTICES for the front-end (npm dependencies)

--- master/front-end/vite.config.ts
+++ master/front-end/vite.config.ts
@@ -19,10 +19,11 @@ import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 
 import * as path from "path";
+import license from "rollup-plugin-license";
 import { defineConfig, loadEnv } from "vite";
 import svgr from "vite-plugin-svgr";
 
-export default defineConfig(() => {
+export default defineConfig(({ command }) => {
     // we have to load the .env file manually here since vite interprets the .env after the config getting loaded
     const env = loadEnv("", process.cwd(), "");
     const apiTarget = env.VITE_LOCAL_API_URL ?? "http://localhost:8080";
@@ -35,6 +36,38 @@ export default defineConfig(() => {
             }),
             svgr(),
         ],
+        build: {
+            rollupOptions: {
+                plugins:
+                    command === "build"
+                        ? [
+                              license({
+                                  thirdParty: {
+                                      includePrivate: false,
+                                      output: {
+                                          file: path.resolve(
+                                              __dirname,
+                                              "reports/dependency-license/THIRD-PARTY-NOTICES.txt",
+                                          ),
+                                          template(dependencies) {
+                                              const separator = "-".repeat(80);
+
+                                              return dependencies
+                                                  .map((dep) => {
+                                                      const notice = dep.noticeText ? `\n\n${dep.noticeText}` : "";
+                                                      const text = dep.licenseText ? `\n\n${dep.licenseText}` : "";
+
+                                                      return `${dep.name}@${dep.version}\nLicense: ${dep.license}${text}${notice}`;
+                                                  })
+                                                  .join(`\n\n${separator}\n\n`);
+                                          },
+                                      },
+                                  },
+                              }),
+                          ]
+                        : [],
+            },
+        },
         server: {
             port: 3000,
             proxy: {
