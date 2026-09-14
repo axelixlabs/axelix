c12ac4df66cc516838cd0acd3f1453614f57e322
Mikhail Polivakha <68962645+mipo256@users.noreply.github.com>
2026-09-14 13:16:40 +0300

    added package-info.java classes for ecosystem packages

--- master/src/main/java/com/axelixlabs/axelix/master/service/ecosystem/projects/package-info.java
+++ master/src/main/java/com/axelixlabs/axelix/master/service/ecosystem/projects/package-info.java
@@ -16,6 +16,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 @NullMarked
-package com.axelixlabs.axelix.master.service.ecosystem;
+package com.axelixlabs.axelix.master.service.ecosystem.projects;
 
 import org.jspecify.annotations.NullMarked;
